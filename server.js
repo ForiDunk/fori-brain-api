2644c7a553bd02756ecd1ca443bbd0c8cfb02232
ForiDunk <szabolcs.forreiter@gmail.com>
2019-04-27 16:30:51 +0300

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-defined-14995',
     user: 'fori',
     password: '1910403142614Fori',
     database: 'fori-brain',
