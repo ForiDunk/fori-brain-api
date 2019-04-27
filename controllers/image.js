36071182ebb57cce8fd659c4e038f96c45c0b7ed
ForiDunk <szabolcs.forreiter@gmail.com>
2019-04-27 10:43:36 +0300

    security refactor

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'e0f93c0e978d48c1ba4722f3993164eb',
+});
+
+const handleApiCall = () => (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with api'));
+};
+
 const handleImage = db => (req, res) => {
   const { id } = req.body;
   db('users')
@@ -12,4 +27,5 @@ const handleImage = db => (req, res) => {
 
 module.exports = {
   handleImage: handleImage,
+  handleApiCall: handleApiCall,
 };
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,9 +1,13 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
   db.select('email', 'hash')
     .from('login')
-    .where({ email: req.body.email })
+    .where({ email })
     .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db
           .select('*')
